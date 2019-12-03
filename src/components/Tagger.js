805ef35ac804daa2b59625f5ca1e257f4d82981b
Davi Bortolotti <davi.bortolotti@fu2re.com.br>
2019-12-03 19:23:35 -0300

    Fixes onClick for modal. Adds delete button

--- src/components/Tagger.js
+++ src/components/Tagger.js
@@ -11,7 +11,6 @@ export default class Tagger extends Component {
       modal: false,
       modalPosition: { top: null, left: null }
     };
-    this.setTag = this.setTag.bind(this);
   }
 
   checkIfTagAlreadyExists = (list, newElement) => {
@@ -24,26 +23,11 @@ export default class Tagger extends Component {
 
   showModal = () => {
     this.setState({ modal: true });
-    console.log("modal");
   };
 
   hideModal = () => {
     this.setState({ modal: false });
-    console.log("modal off");
   };
-
-  // updateDimensions = (e) => {
-  //     var windowX = window.clientX;
-
-  //     if (this.state.modalPosition & this.state.modalPosition.left + 400 < windowX) {
-  //         console.log('foi')
-  //         this.setState( (prevState) => {
-  //             this.setState({modalPosition: {top: prevState.top, left: prevState.left - 400}})
-  //         })
-  //     }
-
-  //     //
-  // };
 
   measureElement = element => {
     const DOMNode = ReactDOM.findDOMNode(element);
@@ -54,55 +38,53 @@ export default class Tagger extends Component {
   };
 
   onSelect = e => {
-    if (e.target.nodeName == "MARK") {
+    var baseIndex = window.getSelection().baseOffset;
+    var lastIndex = window.getSelection().focusOffset;
+    var wordSize = lastIndex - baseIndex;
+    var elem = window.getSelection().baseNode.parentNode;
+    var offset = window.getSelection().baseOffset;
+    console.log(elem.nodeName);
+
+    if ((wordSize == 0) & (elem.nodeName !== "MARK")) {
+      this.hideModal();
+      return;
+    }
+
+    if (
+      ((elem.parentNode.nodeName == "MARK") & (elem.nodeName == "SPAN")) |
+      (elem.nodeName == "MARK")
+    ) {
       var siblingOffset = 0;
       siblingOffset = this.calculateSiblingsOffset(
         e.target.parentNode,
         siblingOffset
       );
-      this.setState(prevState => {
-        return {
-          selected: prevState.selected.filter(e => e.start !== siblingOffset)
-        };
-        this.setState({ selectedExistant: true });
-      });
-    }
-
-    var baseIndex = window.getSelection().baseOffset;
-    var lastIndex = window.getSelection().focusOffset;
-    var wordSize = lastIndex - baseIndex;
-    if (wordSize <= 0) {
-      this.hideModal();
-      return;
-    }
-    var elem = window.getSelection().baseNode.parentNode;
-    var offset = window.getSelection().baseOffset;
-    if (
-      ((elem.parentNode.nodeName == "MARK") & (elem.nodeName == "SPAN")) |
-      (elem.nodeName == "MARK")
-    ) {
-      return;
+      if (!this.state.selectForDelete) {
+        this.setState(prevState => {
+          return {
+            selectForDelete: prevState.selected.filter(
+              e => e.start === siblingOffset
+            )[0]
+          };
+        });
+      } else if (wordSize <= 0) {
+        this.hideModal();
+        this.setState({ selectForDelete: null, selection: null });
+        return;
+      }
     }
 
     var xPosition = e.clientX;
     var yPosition = e.clientY;
     var left = xPosition;
     var right = "none";
-    // console.log(xPosition);
 
     this.showModal();
-
-    // console.log(modalWidth);
-    // // console.log(window.innerWidth)
-    // if ((xPosition + self.modalWidth) > window.innerWidth) {
-    //     left = 'none';
-    //     right = 0;
-    //     // console.log('foi')
-    // }
 
     this.setState({
       modalPosition: { top: yPosition, left: left, right: right }
     });
+
     offset = this.calculateSiblingsOffset(elem, offset);
     var start = offset;
     var end = offset + wordSize;
@@ -119,15 +101,7 @@ export default class Tagger extends Component {
     return offset;
   };
 
-  setTag(e) {
-    this.setState(
-      {
-        test: e.target.value
-      },
-      () => {
-        console.log(this.state);
-      }
-    );
+  setTag = e => {
     var newSelected = {
       start: this.state.selection.start,
       end: this.state.selection.end,
@@ -138,7 +112,24 @@ export default class Tagger extends Component {
       selected: [...prevState.selected, newSelected].sort(this.sortTags)
     }));
     this.hideModal();
-  }
+  };
+
+  deleteTag = () => {
+    console.log(this.state);
+    var toDelete = {
+      start: this.state.selectForDelete.start,
+      end: this.state.selectForDelete.end
+    };
+    console.log(toDelete);
+
+    this.setState(prevState => ({
+      selected: prevState.selected.filter(
+        e => (e.start !== toDelete.start) & (e.end !== toDelete.end)
+      )
+    }));
+    this.setState({ selectForDelete: null });
+    this.hideModal();
+  };
 
   sortTags = (a, b) => {
     if (a.start > b.start) {
@@ -153,35 +144,30 @@ export default class Tagger extends Component {
   render() {
     const spans = this.state.selected;
 
-    const ents = [
-      { type: "g-struc", color: { r: 166, g: 226, b: 45 } },
-      { type: "prop", color: { r: 67, g: 198, b: 252 } },
-      { type: "value", color: { r: 47, g: 187, b: 171 } },
-      { type: "citation", color: { r: 255, g: 102, b: 255 } },
-      { type: "fig-ref", color: { r: 204, g: 153, b: 0 } }
-    ];
-
     var modal = null;
-    console.log(this.state.modal);
+    var deleteTag = null;
+    if (this.state.selectForDelete) {
+      deleteTag = this.deleteTag;
+    }
     if (this.state.modal === true) {
       modal = (
         <TaggerModal
           position={this.state.modalPosition}
           handler={this.setTag}
-          ents={ents}
+          deleteTag={deleteTag}
+          ents={this.props.ents}
           ref={r => (this.tagRef = r)}
+          hideModal={this.hideModal}
         />
       );
     }
 
     return (
-      <Container
-        fluid={true}
-        onMouseUp={this.onSelect}
-        className="white_taggy_text"
-      >
+      <Container fluid={true} className="white_taggy_text">
         {modal}
-        <Taggy text={this.props.text} spans={spans} ents={ents} />
+        <div onMouseUp={this.onSelect}>
+          <Taggy text={this.props.text} spans={spans} ents={this.props.ents} />
+        </div>
       </Container>
     );
   }
--- src/components/TaggerModal.js
+++ src/components/TaggerModal.js
@@ -1,49 +1,69 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
 
-
-import './TaggerModal.css'
+import "./TaggerModal.css";
 
 export default class TaggerModal extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (
+      this.ref.offsetWidth + parseInt(this.ref.style.left, 10) >
+      window.innerWidth
+    ) {
+      this.ref.style.left = null;
+      this.ref.style.right = 0;
+    }
+  }
+  render() {
+    const buttons = this.props.ents.map(element => {
+      var name = element.type;
+      var colors = element.color;
+      var backgroundColor = `rgb(${colors.r}, ${colors.g}, ${colors.b})`;
+      return (
+        <Button
+          className="tag_button"
+          onClick={this.props.handler}
+          value={name}
+          style={{ backgroundColor }}
+          key={name}
+        >
+          {name.toUpperCase()}
+        </Button>
+      );
+    });
+
+    var deleteButton = null;
+    if (this.props.deleteTag) {
+      deleteButton = (
+        <Button
+          className="delete_button"
+          onClick={this.props.deleteTag}
+          value="delete"
+          key="delete"
+          style={{ backgroundColor: "red" }}
+        >
+          X
+        </Button>
+      );
     }
 
-    componentDidMount() {
-        if (this.ref.offsetWidth + parseInt(this.ref.style.left, 10) > window.innerWidth) {
-            this.ref.style.left = null;
-            this.ref.style.right = 0;
-        }
-    }
-    render() { 
-        
+    return (
+      <div className="tagger_modal" onClick={this.props.hideModal}>
+        <div
+          className="tagger_modal_main"
+          style={this.props.position}
+          ref={r => (this.ref = r)}
+        >
+          <div>
+            {buttons}
 
-        const buttons = this.props.ents.map(element => {
-            var name = element.type;
-            var colors = element.color;
-            var backgroundColor = `rgb(${colors.r}, ${colors.g}, ${colors.b})`
-            return <Button className='tag_button' 
-                           onClick={this.props.handler} 
-                           value={name}
-                           style={{backgroundColor}}
-                           key={name}
-                >{name.toUpperCase()}</Button>
-        });
-        
-        
-        return (
-            <div className='tagger_modal'>
-                <div className='tagger_modal_main' style={this.props.position} ref={r => this.ref = r}>
-                    <div>
-                        {buttons}
-                        <Button className='tag_button' 
-                           onClick={this.props.deleteTag} 
-                           value='delete'
-                           key='delete'
-                           style={{backgroundColor: 'red'}}
-                            >X</Button>
-                    </div>
-                </div>
-            </div>);
-    }
+            {deleteButton}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
